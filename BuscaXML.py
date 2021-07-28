ba33929797bc22436a9b561ea3978209e7d8298f
Tuliocafe <tuliocafe@yahoo.com.br>
2021-07-27 22:12:30 -0300

    Versao 1.5
    - Corrigido busca de XML quando o mesmo continha pedido de inutilização ‘-ped-inu’.
    
    Atualização 1.04
    
    - Ajustado busca dos arquivos XML pelo parâmetro do TSEmissorNFCe.ini,
    Se o sistema não encontrar o arquivo, ele vai buscar na pasta c:\Mafra\XML
    
    Atualização 1.03
    
    - Agora o sistema busca somente arquivos que contem no final do nome  ‘-nfe’ ou  ‘-inu’.

--- BuscaXML.py
+++ BuscaXML.py
@@ -9,6 +9,20 @@ caminho_procura = r'c:\Mafra\XML'
 caminho_novo = r'c:\Mafra\XML_Novo'
 
 
+try:
+    manipulador = open('C:\Program Files (x86)\TronSolution\TSEmissorNFCe.ini', 'r')
+    for c in range(0, 8):
+        manipulador.readline()
+    caminho_procura = manipulador.readline()[8:]
+    caminho_procura = repr(caminho_procura.encode('utf-8', 'ignore'))[2:-2]
+    manipulador.close()
+
+except FileNotFoundError:
+    pass
+
+print(f'local do arquivo é:   {caminho_procura}')
+
+
 def sequencia():
     serie = str(input('Digite a SERIE do XML ou pressione ENTER para 002: '))
     if serie == '':
@@ -19,15 +33,18 @@ def sequencia():
 
     elif len(serie) >= 4:
         print('Digito errado, tente novamente! ')
+        sequencia()
 
     else:
         print('Numero digitado errado')
         sequencia()
+
     print('')
     print('Deve colocar o NFCE inicial e o final ex: "30 a 40"')
     print('')
     numero = str(input('Qual sequencia (ex: xxx YYY) ? '))
     listanfcetemp = re.sub("[^0-9]", " ", numero).strip().split(' ')
+
 
     for n in range(0, len(listanfcetemp)):
         if listanfcetemp[n].isdigit():
@@ -54,10 +71,11 @@ def localizarxml():
 
     elif len(serie) >= 4:
         print('Digito errado, tente novamente! ')
+        localizarxml()
 
     else:
         print('Numero digitado errado')
-
+        localizarxml()
 
     listanfce.clear()
     print('')
@@ -75,9 +93,6 @@ o sistema vai ignorar letras e buscar todos os numeros que foram inceridos""")
 
 
 def copiararquivos(nfce):
-
-
-
     for item in nfce:
         print(f'Procurando item {item}')
     print('')
@@ -86,38 +101,48 @@ def copiararquivos(nfce):
     except FileExistsError:
         pass
 
-    cont = 0
-    for raiz, diretorios, arquivos in os.walk(caminho_procura):
-        for arquivo in arquivos:
-            print(arquivo)
-            for x in nfce:
-                if x in arquivo:
+    encontrado = 0
+    nao_encontrado = 0
+    listanecontrado = []
+    for x in nfce:
+        for raiz, diretorios, arquivos in os.walk(caminho_procura):
+            for arquivo in arquivos:
+                if x in arquivo and '-nfe' in arquivo or x in arquivo and '-inu' in arquivo and '-ped' not in arquivo:
                     caminho_antigo = os.path.join(raiz, arquivo)
-                    print(f' {cont +1} XML Encontrados: {caminho_antigo}  ')
-                    mov_arquivonovo = os.path.join(caminho_novo, arquivo)
-                    shutil.copy(caminho_antigo, mov_arquivonovo)
-                    cont += 1
+                    print(f' {encontrado +1} XML Encontrados: {caminho_antigo}  ')
+                    arquivonovo = os.path.join(caminho_novo, arquivo)
+                    shutil.copy(caminho_antigo, arquivonovo)
+                    encontrado += 1
+            if nao_encontrado != encontrado:
+                nao_encontrado = encontrado
+            else:
+                listanecontrado.append(x)
+
 
     print('')
-    if cont == 0:
+    if encontrado == 0:
         print('=-'*20)
         print('      NENHUM XML encontrado')
         print('=-' * 20)
-    if cont != 0:
+    if encontrado != 0:
         os.startfile(caminho_novo)
+        print('Item Não encontrados: ')
+        for num, item in enumerate(listanecontrado, start=1):
+            print(f'{num} - {item}')
 
 
 if os.path.exists(caminho_procura) == True:
     while True:
         print("""    ****************************** 
-    ******   BuscaXML  V 1.0 *****
+    ******   BuscaXML  V 1.5 *****
     ******************************
     ** MENU **** By Tulio Cafe ***
+    
     1 Busca XML Individual
     2 Busca XML em serie
     3 Sair""")
 
-        menu = int(input('Digite o numero: '))
+        menu = int(input('Digite a Opção desejada: '))
 
         try:
             if menu == 1:
@@ -137,5 +162,5 @@ if os.path.exists(caminho_procura) == True:
             pass
 
 else:
-    print('Pasta C:\\mafra\\xml nao encontada')
+    print(f'{caminho_procura} nao encontada')
     exit = input('Pressione ENTER para fechar')
